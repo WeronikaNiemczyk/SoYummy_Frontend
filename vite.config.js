7a1b357e821415d035d7481b0590182a392e573b
Marek Lempart <kama.zakupy@gmail.com.com>
2024-08-05 09:34:58 +0200

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+// import dotenv from "dotenv";
+import envCompatible from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/SoYummy_FrontEnd_groupNo_1/",
-  plugins: [react()],
+  plugins: [react(), envCompatible()],
 });
