957bf075a0e0d62ea4765e5182112b32b21c8709
Marek Lempart <kama.zakupy@gmail.com.com>
2024-08-05 20:17:38 +0200

    fix

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,8 +1,8 @@
 // src/pages/WelcomePage.jsx
 
 import AuthNav from "../components/AuthNav.jsx";
-import css from "../styles/WelcomePage.module.css";
 import symbolDefs from "../images/symbol-defs.svg";
+import css from "../styles/WelcomePage.module.css";
 
 const WelcomePage = () => {
   return (
@@ -16,11 +16,7 @@ const WelcomePage = () => {
           </div>
           <div className={css.welcomeText}>
             <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
-            <p className={css.welcomeSubtitle}>
-              This app offers more than just a collection of recipes - it is
-              designed to be your very own digital cookbook. You can easily save
-              and retrieve your own recipes at any time..
-            </p>
+            <p className={css.welcomeSubtitle}>This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and retrieve your own recipes at any time..</p>
           </div>
 
           <AuthNav />
@@ -28,22 +24,23 @@ const WelcomePage = () => {
       </div>
 
 
-// import AuthNav from "../components/AuthNav.jsx";
-// import css from "../styles/WelcomePage.module.css";
 
-// const WelcomePage = () => {
-//  return (
- //   <div className={css.welcomeContainer}>
-  //    <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
 
-    //  <p className={css.welcomeSubtitle}>
-    //    This app offers more than just a collection of recipes - it is designed
-    //    to be your very own digital cookbook. You can easily save and retrieve
-    //    your own recipes at any time..
-    //  </p>
-    //  <AuthNav />
-  //  </div>
-//  );
-// };
 
-export default WelcomePage;
+ {/* import AuthNav from "../components/AuthNav.jsx";
+ import css from "../styles/WelcomePage.module.css";
+
+const WelcomePage = () => {
+ return (
+   <div className={css.welcomeContainer}>
+    <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
+
+  <p className={css.welcomeSubtitle}>
+    This app offers more than just a collection of recipes - it is designed
+    to be your very own digital cookbook. You can easily save and retrieve
+    your own recipes at any time..
+  </p>
+  <AuthNav />
+  </div>
+  );
+ }; */}
