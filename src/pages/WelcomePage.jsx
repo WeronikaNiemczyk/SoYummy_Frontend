a258561288fbef1bb5c29dd367ae0be14a3a6ffd
kruzelgo <kruzelgo@gmail.com>
2024-08-05 19:57:03 +0200

    WelcomePage small changes

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -4,23 +4,26 @@ import symbolDefs from "../images/symbol-defs.svg";
 
 const WelcomePage = () => {
   return (
-    <div className={css.welcomeContainer}>
-      <div className={css.logoTextContainer}>
-        <div className={css.welcomeLogo}>
-          <svg>
-            <use xlinkHref={`${symbolDefs}#icon-logo`} />
-          </svg>
-        </div>
-        <div className={css.welcomeText}>
-          <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
-          <p className={css.welcomeSubtitle}>
-            This app offers more than just a collection of recipes - it is
-            designed to be your very own digital cookbook. You can easily save
-            and retrieve your own recipes at any time..
-          </p>
+    <div className={css.welcomePageContainer}>
+      <div className={css.welcomeContainer}>
+        <div className={css.logoTextContainer}>
+          <div className={css.welcomeLogo}>
+            <svg>
+              <use xlinkHref={`${symbolDefs}#icon-logo`} />
+            </svg>
+          </div>
+          <div className={css.welcomeText}>
+            <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
+            <p className={css.welcomeSubtitle}>
+              This app offers more than just a collection of recipes - it is
+              designed to be your very own digital cookbook. You can easily save
+              and retrieve your own recipes at any time..
+            </p>
+          </div>
+
+          <AuthNav />
         </div>
       </div>
-      <AuthNav />
     </div>
   );
 };
