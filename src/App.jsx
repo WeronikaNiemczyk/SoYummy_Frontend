83f614494457129b5b17227d64dc2a9f67323b96
Marek Lempart <kama.zakupy@gmail.com.com>
2024-08-05 10:04:27 +0200

    Header launch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
-// // src/App.jsx
+// src/App.jsx
+
+import { Header } from "./components/Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Inne komponenty */}
+    </div>
+  );
+};
+
+export default App;
 
 // import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 // import AddRecipes from "./pages/AddRecipes";
@@ -114,71 +127,71 @@
 
 // export default App;
 
-// src/App.jsx
+// // src/App.jsx
 
-import {
-  Link,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import AddRecipes from "./pages/AddRecipes";
-import Categories from "./pages/Categories";
-import Favorites from "./pages/Favorites";
-import Home from "./pages/Home";
-import LandingPage from "./pages/LandingPage";
-import Login from "./pages/Login";
-import MyRecipes from "./pages/MyRecipes";
-import Register from "./pages/Register";
-import Search from "./pages/Search";
-import ShoppingList from "./pages/ShoppingList";
-import User from "./pages/User";
-import styles from "./styles/App.module.css";
+// import {
+//   Link,
+//   Route,
+//   BrowserRouter as Router,
+//   Routes,
+//   useLocation,
+// } from "react-router-dom";
+// import AddRecipes from "./pages/AddRecipes";
+// import Categories from "./pages/Categories";
+// import Favorites from "./pages/Favorites";
+// import Home from "./pages/Home";
+// import LandingPage from "./pages/LandingPage";
+// import Login from "./pages/Login";
+// import MyRecipes from "./pages/MyRecipes";
+// import Register from "./pages/Register";
+// import Search from "./pages/Search";
+// import ShoppingList from "./pages/ShoppingList";
+// import User from "./pages/User";
+// import styles from "./styles/App.module.css";
 
-const AppContent = () => {
-  const location = useLocation();
-  const showNav = location.pathname !== "/";
+// const AppContent = () => {
+//   const location = useLocation();
+//   const showNav = location.pathname !== "/";
 
-  return (
-    <div className={styles.app}>
-      {showNav && (
-        <nav className={styles.navbar}>
-          <Link to="/home">Home</Link>
-          <Link to="/categories">Categories</Link>
-          <Link to="/add-recipes">Add recipes</Link>
-          <Link to="/my-recipes">My recipes</Link>
-          <Link to="/favorites">Favorites</Link>
-          <Link to="/shopping-list">Shopping list</Link>
-          <Link to="/search">Search</Link>
-          <Link to="/user">User</Link>
-        </nav>
-      )}
-      <div style={{ marginTop: "100px" }}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/add-recipes" element={<AddRecipes />} />
-          <Route path="/my-recipes" element={<MyRecipes />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/shopping-list" element={<ShoppingList />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/user" element={<User />} />
-        </Routes>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.app}>
+//       {showNav && (
+//         <nav className={styles.navbar}>
+//           <Link to="/home">Home</Link>
+//           <Link to="/categories">Categories</Link>
+//           <Link to="/add-recipes">Add recipes</Link>
+//           <Link to="/my-recipes">My recipes</Link>
+//           <Link to="/favorites">Favorites</Link>
+//           <Link to="/shopping-list">Shopping list</Link>
+//           <Link to="/search">Search</Link>
+//           <Link to="/user">User</Link>
+//         </nav>
+//       )}
+//       <div style={{ marginTop: "100px" }}>
+//         <Routes>
+//           <Route path="/" element={<LandingPage />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/categories" element={<Categories />} />
+//           <Route path="/add-recipes" element={<AddRecipes />} />
+//           <Route path="/my-recipes" element={<MyRecipes />} />
+//           <Route path="/favorites" element={<Favorites />} />
+//           <Route path="/shopping-list" element={<ShoppingList />} />
+//           <Route path="/search" element={<Search />} />
+//           <Route path="/user" element={<User />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// };
 
-const App = () => {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-};
+// const App = () => {
+//   return (
+//     <Router>
+//       <AppContent />
+//     </Router>
+//   );
+// };
 
-export default App;
+// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
@@ -11,9 +11,11 @@ import { persistor, store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <BrowserRouter basename="/SoYummy_FrontEnd_groupNo_1">
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>
 );
 
