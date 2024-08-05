373dcb87324c968325501924d9b3aadf3518732f
kruzelgo <kruzelgo@gmail.com>
2024-08-05 20:32:37 +0200

    conflicts fix

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,5 +1,3 @@
-// src/pages/WelcomePage.jsx
-
 import AuthNav from "../components/AuthNav.jsx";
 import css from "../styles/WelcomePage.module.css";
 import symbolDefs from "../images/symbol-defs.svg";
@@ -26,24 +24,7 @@ const WelcomePage = () => {
           <AuthNav />
         </div>
       </div>
-
-
-// import AuthNav from "../components/AuthNav.jsx";
-// import css from "../styles/WelcomePage.module.css";
-
-// const WelcomePage = () => {
-//  return (
- //   <div className={css.welcomeContainer}>
-  //    <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
-
-    //  <p className={css.welcomeSubtitle}>
-    //    This app offers more than just a collection of recipes - it is designed
-    //    to be your very own digital cookbook. You can easily save and retrieve
-    //    your own recipes at any time..
-    //  </p>
-    //  <AuthNav />
-  //  </div>
-//  );
-// };
-
+    </div>
+  );
+};
 export default WelcomePage;
