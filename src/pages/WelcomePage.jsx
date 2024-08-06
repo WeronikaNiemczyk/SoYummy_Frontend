90e1b0aad5969fd4b30d3ec632db8f13855fc641
kruzelgo <kruzelgo@gmail.com>
2024-08-06 09:05:38 +0200

    WelcomePage logo fix

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -7,20 +7,23 @@ const WelcomePage = () => {
     <div className={css.welcomePageContainer}>
       <div className={css.welcomeContainer}>
         <div className={css.logoTextContainer}>
-          <div className={css.welcomeLogo}>
-            <svg>
+          <div className={css.welcomeLogoBackground}>
+            <svg className={css.welcomeLogoSVG}>
               <use xlinkHref={`${symbolDefs}#icon-logo`} />
             </svg>
           </div>
           <div className={css.welcomeText}>
             <h1 className={css.welcomeTitle}>Welcome to the app!</h1>
-            <p className={css.welcomeSubtitle}>This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and retrieve your own recipes at any time..</p>
+            <p className={css.welcomeSubtitle}>
+              This app offers more than just a collection of recipes - it is
+              designed to be your very own digital cookbook. You can easily save
+              and retrieve your own recipes at any time..
+            </p>
           </div>
 
           <AuthNav />
         </div>
       </div>
-
     </div>
   );
 };
