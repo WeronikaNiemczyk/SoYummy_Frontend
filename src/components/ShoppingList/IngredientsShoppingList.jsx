3fe5692c899e97de8c821a47e780f85f34873f50
MMolik <molikos@o2.pl>
2024-08-11 20:19:04 +0200

    MM-ShoppingList-CCS

--- src/components/ShoppingList/IngredientsShoppingList.jsx
+++ src/components/ShoppingList/IngredientsShoppingList.jsx
@@ -1,41 +1,53 @@
 import React from "react";
+import css from "../../styles/SchoppingList.module.css";
 
-const IngredientsShoppingList = ({ ingredients, onRemove, onAdd }) => {
+const IngredientsShoppingList = ({ ingredients, onRemove }) => {
   if (!Array.isArray(ingredients)) {
     return <p>Brak składników.</p>;
   }
 
   return (
-    <ul>
-      {ingredients.length === 0 ? (
-        <p>Brak składników na liście.</p>
-      ) : (
-        ingredients.map((ingredient) => (
-          <li key={ingredient._id}>
-            <img
-              src={ingredient.image || "domyślny-obrazek-url"}
-              alt={ingredient.name}
-            />
-            <span>{ingredient.name}</span>
-            <span>
-              {ingredient.quantity} {ingredient.unit}
-            </span>
-            <button onClick={() => onRemove(ingredient._id)}>Usuń</button>
-            <button
-              onClick={() =>
-                onAdd(
-                  "recipeId-placeholder",
-                  ingredient._id,
-                  ingredient.measure
-                )
-              } // Przykład użycia; recipeId i measure muszą być dostosowane
-            >
-              Dodaj
-            </button>
-          </li>
-        ))
-      )}
-    </ul>
+    <div className={css.SchoppingListMainContainer}>
+      <header className={css.SchoppingListHeader}>
+        <h1>Shopping List</h1>
+      </header>
+      <table className={css.SchoppingListTable}>
+        <thead>
+          <tr>
+            <th className={css.productHeader}>Product</th>
+            <th className={css.numberHeader}>Number</th>
+            <th className={css.removeHeader}>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ingredients.length === 0 ? (
+            <tr>
+              <td colSpan="3">Brak składników na liście.</td>
+            </tr>
+          ) : (
+            ingredients.map((ingredient) => (
+              <tr key={ingredient._id}>
+                <td className={css.product}>
+                  <img
+                    src={ingredient.image || "domyślny-obrazek-url"}
+                    alt={ingredient.name}
+                  />
+                  {ingredient.name}
+                </td>
+                <td className={css.number}>
+                  <span className={css.numberContent}>
+                    {ingredient.quantity} {ingredient.unit}
+                  </span>
+                </td>
+                <td className={css.remove}>
+                  <button onClick={() => onRemove(ingredient._id)}>X</button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
