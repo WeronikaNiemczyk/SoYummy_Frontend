20c6a5bcad0dc9874a657c5fd204f523624b0525
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-11 21:00:47 +0200

    dodana funkcja wyszukiwania ze strony glownej

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,21 +3,41 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import css from "../styles/Search.module.css";
+import { useEffect } from "react";
 
 const SearchForm = ({ onSubmit }) => {
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const getQueryParam = (param) => {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get(param);
+    };
+
+    const keywordParam = getQueryParam("keyword");
+    if (keywordParam) {
+      setQuery(keywordParam);
+    }
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(query);
     setQuery("");
   };
-
+  
   return (
-    <form className={ css.searchForm} onSubmit={handleSubmit}>
+    <form
+      action="/search"
+      method="GET"
+      className={css.searchForm}
+      onSubmit={handleSubmit}
+    >
       <input
         className={css.searchInput}
         type="text"
+        id="search-input"
+        name="keyword"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search..."
