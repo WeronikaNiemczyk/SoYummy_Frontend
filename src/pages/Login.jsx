ce627d39222390c17beb9848a17c6f0bb5b7a186
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-09-04 14:47:50 +0200

    Login page style update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,67 +1,10 @@
-// import { useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import LoginUser from "../components/LoginUser";
-// import cookies from "../features/cookies";
-// import styles from "./Login.module.css";
-
-// const Login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [submitted, setSubmitted] = useState(false);
-//   const navigate = useNavigate();
-
-//   const submit = (e) => {
-//     e.preventDefault();
-//     setSubmitted(true);
-//   };
-
-//   const onClick = (e) => {
-//     e.preventDefault();
-//     cookies.setCookie("devToken");
-//     navigate("../SoYummy_Frontend/home");
-//   };
-
-//   return (
-//     <div className={styles.loginPage}>
-//       <form onSubmit={submit}>
-//         <h1>Login</h1>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <div className={styles.buttons}>
-//           <Link to="/">
-//             <button type="button">Cancel</button>
-//           </Link>
-//           <button type="submit">Login</button>
-//         </div>
-//       </form>
-//       {submitted && <LoginUser loginUserData={{ email, password }} />}
-//       <Link to="../SoYummy_Frontend/home">
-//         <button className={styles.devSkip} onClick={onClick}>
-//           Dev - Skip ahead
-//         </button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import LoginUser from "../components/LoginUser";
 import cookies from "../features/cookies";
 import symbolDefs from "../images/symbol-defs.svg";
 import styles from "./Login.module.css";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -79,7 +22,7 @@ const Login = () => {
   return (
     <div className={styles.loginPage}>
       <form className={styles.authForm} onSubmit={submit}>
-        <h2 className={styles.authFormTitle}>Login</h2>
+        <h1>Login</h1>
         <div className={styles.inputGroup}>
           <svg className={styles.icon}>
             <use href={`${symbolDefs}#icon-mail`} />
